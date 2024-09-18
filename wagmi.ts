3771e5b21f51054f19247bf408c61398e9b99d8f
darshitbhalodi5 <darshitbhalodi@gmail.com>
2024-09-18 17:34:53 +0530

    Added network configuration

--- wagmi.ts
+++ wagmi.ts
@@ -1,9 +1,26 @@
-import { getDefaultConfig } from "@rainbow-me/rainbowkit";
-import { mainnet, holesky } from "wagmi/chains";
+import { getDefaultConfig, Chain } from "@rainbow-me/rainbowkit";
+import { bitTorrent, sepolia } from "wagmi/chains";
+
+const myCustomChain: Chain = {
+  id: 1029, 
+  name: 'BitTorrent Chain Donau',
+  nativeCurrency: {
+    decimals: 18,
+    name: 'BTT',
+    symbol: 'BTT',
+  },
+  rpcUrls: {
+    public: { http: ['https://pre-rpc.bt.io/'] },
+    default: { http: ['https://pre-rpc.bt.io/'] },
+  },
+  blockExplorers: {
+    default: { name: 'BTT Scan', url: 'https://testscan.bt.io' },
+  },
+}
 
 export const config = getDefaultConfig({
   appName: "RainbowKit demo",
   projectId: "f8a6524307e28135845a9fe5811fcaa2",
-  chains: [mainnet, holesky],
+  chains: [sepolia, bitTorrent, myCustomChain],
   ssr: true,
 });
